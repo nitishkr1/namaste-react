6e08a5efa3b808d60f319c2332cd294439a8da4b
Nitish Kumar <nitishk1947@gmail.com>
2024-04-11 03:11:57 +0530

    episode 04

--- App.js
+++ App.js
@@ -1,24 +1,64 @@
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement("div", { id: "child1", key:"child1" }, [
-    React.createElement("h1", { key: "h1" }, "Hi i am H1"),
-    React.createElement("h2", { key: "h2" }, "I'm H2"),
-  ]),
-  React.createElement("div", { id: "child2", key:"child2" }, [
-    React.createElement("h1", { key: "h3" }, "Hi i am H1"),
-    React.createElement("h2", { key: "h4" }, "I'm H2"),
-  ])]
-);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello World from React!"
-);
+const Header = () => {
+  return (
+  <div className='header-container'>
+    <div className='logo-container'>
+      <img src='https://www.creativefabrica.com/wp-content/uploads/2023/06/10/Fast-Food-Burger-Restaurant-Logo-Graphics-71757752-1.jpg' className='logo' />
+    </div>
+    <div className='menu-container'>
+      <ul className='menu-list'>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Contact Us</li>
+        <li>Cart</li>
+      </ul>
+    </div>
+  </div>
+  )
+}
 
-console.log(parent);
+const RestaurantCard = () => {
+  return (
+    <div className='res-card'>
+      <img src='https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/x7bcibhxfnrupph6bcft' className='res-logo' alt='res-logo' />
+      <h3>The Burger Club</h3>
+      <h4>American, Beverages, Desserts</h4>
+      <h4>4.1 Star 35-40 mins</h4>
+      <h4>Karol Bagh</h4>
+    </div>
+  )
+}
+
+
+const Body = () => {
+  return (
+    <div className='body'>
+      <div className='search'>Search</div>
+      <div className='res-container'>
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+        <RestaurantCard />
+      </div>
+    </div>
+  )
+}
+
+const AppLayout = () => {
+  return (
+  <div className='app'>
+    <Header />
+    <Body />
+  </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Namaste React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Spliting
+- Differential Bundling - support older browsers
+- Diagostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
